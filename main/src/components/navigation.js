227bcaea30379647a7a127cff6640b5b8e02f007
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-04-28 22:22:44 -0500

    connect components and index

--- main/src/components/navigation.js
+++ main/src/components/navigation.js
@@ -9,14 +9,42 @@ li:{
 }
 
 
-export default function Navigation() {
+export default function Navigation({currentPage, handlePageChange}) {
     return (
         <nav className="navbar" >
             <ul className="navlist" style={styles.li}>
-                <li>About Me</li>
-                <li>Portfolio</li>
-                <li>Contact Me</li>
-                <li>Resume</li>
+                <li>
+                    <a href="#home"
+                    onClick={() => handlePageChange('Home')}
+                    className={currentPage ==='Home' ? 'home-active': 'home-inactive'}
+                    >
+                        About Me
+                    </a>
+                </li>
+                <li>
+                    <a href="#porfolio"
+                    onClick={() => handlePageChange ('Portfolio')}
+                    className={currentPage==='Portfolio' ? 'portfolio-active':'portfolio-inactive'}
+                    >
+                    Portfolio
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#contactme"
+                    onClick={() => handlePageChange ('ContactMe')}
+                    className={currentPage==='ContactMe' ? 'contactme-active':'contactme-inactive'}
+                    >
+                    Contact Me
+                    </a>
+                    </li>
+                    <li>
+                    <a href="#resume"
+                    onClick={() => handlePageChange ('Resume')}
+                    className={currentPage==='Resume' ? 'resume-active':'resume-inactive'}
+                    >
+                    Resume
+                    </a>
+                    </li>
             </ul>
         </nav>
     )
