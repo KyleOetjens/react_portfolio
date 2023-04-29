4a1c54b2f6c374332b11113a721a3e69b03a9067
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-04-28 21:54:25 -0500

    update index and app

--- main/src/App.js
+++ main/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './components/navigation';
-import Footer from './components/footer';
+import React from "react";
+import ContentContainer from "./components/contentContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <section>
-        <Navigation />
-      </section>
-      <header className="App-header">
-      </header>
-      <section className='contentSection'>
-        <contentContainer />
-      </section>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
-  );
-}
+const App = () => <ContentContainer />;
 
 export default App;
+
+
+//import logo from './logo.svg';
+// import './App.css';
+// import Navigation from './components/navigation';
+// import Footer from './components/footer';
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <section>
+// //         <Navigation />
+// //       </section>
+// //       <header className="App-header">
+// //       </header>
+// //       <section className='contentSection'>
+// //         <contentContainer />
+// //       </section>
+// //       <footer>
+// //         <Footer />
+// //       </footer>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
