9809c62557934ea28f3e8d651b6c7f58151d22dd
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-04-29 10:20:31 -0500

    contact update

--- main/src/components/contactMe.js
+++ main/src/components/contactMe.js
@@ -1,23 +1,64 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 
-export default function Project() {
+// Here we import a helper function that will check if the email is valid
+// change this to error when message is not filled in
+//import { checkPassword, validateEmail } from '../../utils/helpers';
+
+export default function ContactMe() {
+
+    const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'contact-name') {
+      setName(inputValue);
+    } else if (inputType === 'contact-email') {
+      setEmail(inputValue);
+    } else if (inputType ==='contact-message'){
+      setMessage(inputValue);
+    }
+  };
     return (
-      <div>
-        <h1>Contact Page</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
-      </div>
-    );
+        <div>
+          <p>Contact</p>
+          <form className="form">
+          <input
+              value={name}
+              name="contact-name"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              value={email}
+              name="contact-email"
+              onChange={handleInputChange}
+              type="email"
+              placeholder="email"
+            />
+            <input
+              value={message}
+              name="contact-message"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Leave a note"
+            />
+            <button type="button" /*onClick={handleFormSubmit}*/>Submit</button>
+          </form>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+        </div>
+      );
   }
